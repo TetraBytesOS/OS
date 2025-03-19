61de532c447080f72703a1f924916a672bd63261
TetraBytesOS <grupofso2025@gmail.com>
2025-03-19 10:31:40 +0100

    Update PruebaBash.sh

--- PruebaBash/PruebaBash.sh
+++ PruebaBash/PruebaBash.sh
@@ -3,71 +3,109 @@ history -r ~/.bash_history
 clear
 echo -e "\e[1;7;5;31;42m"
 cat << "EOF"
-  ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒ ░░▒▓█▓▒░░▒▓█▓▒░           /´/     ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░  
-░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒ ░▒▓█▓▒░░▒▓█▓▒░           ,/¯/       ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+  ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒ ░░▒▓█▓▒░░▒▓█▓▒░           /´/     ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒ ░▒▓█▓▒░░▒▓█▓▒░           ,/¯/       ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
 ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░       /´¯/'.'/´¯`·    ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
-░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░       ░▒▓█████▓▒░       /'/../.../.../¨¯\  ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░   ('(..´..´. ¯~/'..')  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  
- ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒ ░▒▓█▓▒░░▒▓█▓▒░   \...........'.../  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░   
-░▒▓█▓▒░       ░▒▓██████▓▒░ ░▒▓██████▓▒░   ░▒▓█▓▒░░▒▓█▓▒░   ''...\.... _.·´  ░▒▓█▓▒░    ░▒▓██████▓▒░ ░▒▓██████▓▒░     
-                                                            \.........(                                              
-                                                             \.........\                                             
+░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░       ░▒▓█████▓▒░       /'/../.../.../¨¯\  ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░   ('(..´..´. ¯~/'..')  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+ ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒ ░▒▓█▓▒░░▒▓█▓▒░   \...........'.../  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░       ░▒▓██████▓▒░ ░▒▓██████▓▒░   ░▒▓█▓▒░░▒▓█▓▒░   ''...\.... _.·´  ░▒▓█▓▒░    ░▒▓██████▓▒░ ░▒▓██████▓▒░
+                                                            \.........(
+                                                             \.........\
 
 EOF
 echo -e "\e[25;39;49m"
-echo -e "\e[1;7;31;42m"
+echo -e "\e[1;7;93;42m"
 
 echo "Pro tip: escribe 'comandos' "
 
 CondicionWhile=0
 while [ "$CondicionWhile" -eq 0 ]; do
-	echo "Introduce comando:"
+        echo "Introduce comando:"
     read miVar  #Introduces Info por la consola
     echo "$miVar" >> fuckHistory.txt #añade lo que hayas escrito en miVar al historial interno
     CondicionLS="salir" #sale del while y de la shell
-    
-    if [[ "$miVar" == "$CondicionLS" ]]; then 
+
+    if [[ "$miVar" == "$CondicionLS" ]]; then
         CondicionWhile=1 #Si introduces Salir se rompe el while
 
-    elif [ "$miVar" == "fecha" ]; then   
- 	date  # Muestra la fecha y hora actual
+    elif [ "$miVar" == "fecha" ]; then
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
+        date  # Muestra la fecha y hora actual
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
 
     elif [ "$miVar" == "mostrar" ]; then #Va a hacer cat a un archivo
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[93;104m"
         echo "Por favor, proporciona un archivo:"
+        echo -e "\e[27;39;49m"
+
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         read miArchivo #Introduces el archivo
-    	cat  $miArchivo #Hace cat al archivo
+        cat  $miArchivo #Hace cat al archivo
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
 
     elif [ "$miVar" == "internalHistory" ]; then #muestra el historial de nuestra shell
-	cat fuckHistory.txt
-    
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
+        cat fuckHistory.txt
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     elif [ "$miVar" == "fuckHistory" ]; then #elimina el historial de nuestra shell
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         echo " " > fuckHistory.txt
-
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     elif [ "$miVar" == "mostrarInfo" ];then #hace neofetch, muestra especificaciones del ordenador,CPU etc
-	echo "Iniciando neofetch..."
-	neofetch
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
+        echo "Iniciando neofetch..."
+        neofetch
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
 
     elif [ "$miVar" == "monitor" ];then #hace htop, muestrprocesos, RAM usandose, uso de nucleos de CPU etc
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         echo "Iniciando htop..."
         htop
-
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     elif [ "$miVar" == "comandos" ];then #muestra los comandos añadidos por nosotros
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         echo "Extra commands: fecha, mostrar, internalHistory, fuckHistory, mostrarInfo, monitor, mostrarPID, crearProceso, matarProceso, comandos, salir"
-
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     elif [[ "$miVar" == "mostrarPID" ]]; then
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         echo "El PID del proceso actual es: $$" #muestra proceso actual
-
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     elif [[ "$miVar" == "crearProceso" ]]; then  #crea un proceso
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         echo "Creando proceso hijo..."
         bash -c 'sleep 60 & echo "Proceso hijo creado con PID: $!"'
-
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     elif [[ "$miVar" == "matarProceso" ]]; then #mata proceso
+        echo -e "\e[21;24;27;39;49m"
+        echo -e "\e[35;106m"
         echo "Ingresa el PID del proceso a matar:"
         read pid
         kill -9 $pid
         echo "Proceso $pid terminado."
+        echo -e "\e[39;49m"
+        echo -e "\e[1;7;93;42m"
     else  #Si introduces un comando ya existente
-	eval $miVar   #Se ejecuta el comando introducido
+        eval $miVar   #Se ejecuta el comando introducido
     fi
 done
 
