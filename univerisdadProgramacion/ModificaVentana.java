6f1f4660e182fbff1dbca8c7752b5072bd3746fd
TetraBytesOS <grupofso2025@gmail.com>
2025-04-23 09:34:51 +0200

    Delete univerisdadProgramacion directory

--- univerisdadProgramacion/ModificaVentana.java
+++
@@ -1,7 +0,0 @@
-package univerisdadProgramacion;
-
-public interface ModificaVentana { //las clases que lo implementen podran modificar la ventana principal
-
-	public void setOnTop(Marcos miMarco,boolean alwaysOnTop); //Para que este por encima del resto
-	public void repaintVentana(Marcos miMarco); //Para cambiar el texto del marco si no funciona el metodo de la clase Textos
-}
